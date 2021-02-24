81d9c305ee02fa368ccb105719f141584341d158
washlopes <washlopes@localhost>
2021-02-24 15:08:08 -0300

    Commit inicial do projeto

---
+++ src/test/java/br/com/vectordev/medical/MedicalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vectordev.medical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
